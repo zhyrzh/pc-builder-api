141b5f9dfd2582290a5a70e6c1939a4dd58f8092
Rhyz <arongrhyz@gmail.com>
2025-06-23 06:52:57 +0800

    bermore techzone scraping flow with the following data scraped: link, orignalName, price, image, brand, productName (modified name)

--- src/index.ts
+++ src/index.ts
@@ -1,176 +1,342 @@
-require("dotenv").config();
-import puppeteer, { Page } from "puppeteer";
-import Groq from "groq-sdk";
+import puppeteer, { Browser, Page } from "puppeteer";
+import { db } from "./db";
+import { tryCatch } from "./utils";
 
-(async () => {
-  const browser = await puppeteer.launch();
+async function initializeScrapingTools(): Promise<Browser> {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
   const page = await browser.newPage();
 
-  await page.goto("https://www.pchubonline.com/products?category=2", {
-    waitUntil: "networkidle0",
+  return browser;
+}
+
+async function getAllData() {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const mainPage = await browser.newPage();
+
+  mainPage.setViewport({
+    width: 1440,
+    height: 1000,
+    deviceScaleFactor: 1,
+    isMobile: false,
+    isLandscape: true,
   });
 
-  const allGpus = await getAllGPUS(page);
+  await mainPage.goto(
+    "https://bermorzone.com.ph/product-category/video-cards/",
+    {
+      waitUntil: "networkidle2",
+      timeout: 0,
+    }
+  );
 
-  if (allGpus !== undefined) {
-    console.log(allGpus.length, "allgpus.lnght");
-    const gpuNames = allGpus.map((itm) => itm?.name);
+  const productsWithLink = await mainPage.$$eval(
+    "li.product.type-product",
+    allProductsEvalCallback
+  );
 
-    const partitionedGpuNames = [];
-    for (let i = 0; i < gpuNames.length; i += 40) {
-      partitionedGpuNames.push(gpuNames.slice(i, i + 20));
+  const withName = productsWithLink.map(async (p, index) => {
+    let productCode = "";
+    const productImageSet = new Set<string>();
+    let productVariations = [];
+    let initialProductInfo = {
+      productName: "",
+      price: "",
+      image: "",
+      brand: "",
+    };
+
+    const productPage = await browser.newPage();
+
+    productPage.setViewport({
+      width: 1440,
+      height: 1000,
+      deviceScaleFactor: 1,
+      isMobile: false,
+      isLandscape: true,
+    });
+
+    productPage.on("request", (request) => {
+      if (request.resourceType() === "image") {
+        productImageSet.add(request.url());
+      }
+    });
+
+    await productPage.goto(p.link, {
+      waitUntil: "networkidle0",
+      timeout: 0,
+    });
+
+    const { data: originalName, error: _productNameError } = await tryCatch(
+      productPage.$eval(
+        "h1.product_title.entry-title",
+        (itm) => itm.textContent
+      )
+    );
+
+    if (originalName !== null) {
+      const hasProductCode = originalName.match(
+        /\b[A-Z]{2,}(?:-[A-Z0-9]{2,}){1,}\b/g
+      );
+      if (hasProductCode !== null && hasProductCode?.length! >= 1) {
+        // console.log(hasProductCode, productNameDirty, "HAS MATCH");
+        productCode = hasProductCode[0];
+      }
+      initialProductInfo.productName =
+        initialRemoveUnnecessaryKeywords(originalName);
+      initialProductInfo.brand = originalName.split(" ")[0];
     }
 
-    console.log(partitionedGpuNames, "partitioned");
-
-    const gpusWithAdditionalDetails = await getProductDetails(
-      partitionedGpuNames[0]
+    await productPage.waitForSelector(
+      "div.woocommerce-product-gallery.woocommerce-product-gallery--with-images div.flex-viewport div[data-thumb] a img[width='600']"
     );
 
-    const promptResult: { gpuDetails: Array<any> } = JSON.parse(
-      gpusWithAdditionalDetails.choices[0].message.content!
+    const { data: productImage } = await tryCatch(
+      productPage.$eval(
+        "div.woocommerce-product-gallery.woocommerce-product-gallery--with-images div.flex-viewport div[data-thumb] a img[width='600']",
+        (itm) => itm.src
+      )
     );
 
-    console.log(promptResult, "promptResult.gpus.length");
-  }
+    if (productImage !== null && productImage !== undefined) {
+      initialProductInfo.image =
+        productImage.split("/")[productImage.split("/").length - 1];
+    }
 
-  await browser.close();
-})();
+    const { data: initialPriceInfoWithDiscount } = await tryCatch(
+      productPage.$eval("p.price span.electro-price ins span bdi", (itm) =>
+        itm.textContent?.toLowerCase()
+      )
+    );
 
-async function getAllGPUS(page: Page): Promise<Array<any> | undefined> {
-  try {
-    const productCardSelector = ".text-center.rounded-lg.bg-white.p-2";
-    const nextButtonSelector =
-      "div.flex.justify-center nav[aria-label='Page navigation example'] li:last-child";
-    let i = 1;
-    let allGPUs: Array<any> = [];
+    const { data: initialPriceInfoNoDiscount } = await tryCatch(
+      productPage.$eval("p.price span.electro-price bdi", (itm) =>
+        itm.textContent?.toLowerCase()
+      )
+    );
 
-    while (true) {
-      const productCount = await page.$$eval(
-        productCardSelector,
-        (products) => products.length
-      );
+    if (
+      initialPriceInfoWithDiscount !== null &&
+      initialPriceInfoWithDiscount !== undefined
+    ) {
+      initialProductInfo.price = initialPriceInfoWithDiscount;
+    }
 
-      if (productCount === 0) {
-        break;
-      }
+    if (
+      initialPriceInfoNoDiscount !== null &&
+      initialPriceInfoNoDiscount !== undefined
+    ) {
+      initialProductInfo.price = initialPriceInfoNoDiscount;
+    }
 
-      let producList = await page.$$eval(productCardSelector, (products) =>
-        products.map((itm) => {
-          const productTitleSelector = ".text-title-2.text-left";
-          const productPriceSelector = "div span.text-md.text-gray-900";
-          const productImageSelector = "img";
-          const productBrandSelector = "div.grid.grid-cols-2 h3";
+    const { data: variations, error: _variationsError } = await tryCatch(
+      productPage.$eval("table.variations th.label", (itm) =>
+        itm.textContent?.toLowerCase()
+      )
+    );
 
-          const getAttributeValue = (
-            selector: string,
-            attribute: string
-          ): string => itm?.querySelector(selector)?.getAttribute(attribute)!;
-
-          const getTextContentValue = (selector: string) =>
-            itm.querySelector(selector)?.textContent!;
-
-          return {
-            imageId: getAttributeValue("a", "id"),
-            name: getTextContentValue(productTitleSelector),
-            price: Number.parseFloat(
-              getTextContentValue(productPriceSelector)
-                ?.replace("₱ ", "")
-                .replace(",", "")!
-            ),
-            image: `https://www.pchubonline.com${getAttributeValue(
-              productImageSelector,
-              "src"
-            )}`,
-            brand: itm
-              .querySelector(productBrandSelector!)
-              ?.textContent?.replace("● ", ""),
-          } as any;
-        })
-      );
-
-      for (let gpu of producList) {
-        await page.click(`a#${gpu.imageId}`);
-
-        const gpuLink = await page.$eval("div#view_product_link a", (el) => {
-          return el !== undefined
-            ? `https://www.pchubonline.com${el?.getAttribute("href")}`
-            : undefined;
-        });
-
-        await page.click(
-          "div.flex.items-start.justify-between.p-4.border-b.rounded-t button"
+    if (variations !== null) {
+      if (
+        variations === "capacity" ||
+        variations === "memory" ||
+        variations === "version"
+      ) {
+        productVariations = await handleMemoryVariation(
+          productPage,
+          variations,
+          originalName!,
+          initialProductInfo
         );
-
-        gpu.link = gpuLink;
+        console.log(productVariations, "product variations");
       }
+    }
 
-      const nextButton = await page.$(nextButtonSelector!);
+    if (productImageSet.size >= 1) {
+      const allImages = [...productImageSet].filter((itm) =>
+        itm.includes(initialProductInfo.image)
+      );
+      initialProductInfo.image = allImages[0];
+    }
 
-      await Promise.all([
-        page.waitForNavigation({ waitUntil: "networkidle0" }),
-        nextButton!.click(),
+    await productPage.close();
+
+    const returnedObj = {
+      ...p,
+      ...initialProductInfo,
+      originalName,
+      productName: finalRemovalUnnecessaryKeywords(
+        initialProductInfo.productName
+      ),
+    };
+
+    if (productVariations.length >= 2) {
+      productVariations = productVariations.map((itm) => ({
+        ...itm,
+        image: initialProductInfo.image,
+      }));
+    }
+
+    return productVariations.length >= 2 ? productVariations : returnedObj;
+  });
+
+  const allRes = await Promise.all(withName);
+  console.log(allRes, "outside");
+  await mainPage.screenshot({
+    path: "mainpage-all-items.png",
+    fullPage: true,
+  });
+}
+
+async function handleMemoryVariation(
+  productPage: Page,
+  variations: string,
+  originalName: string,
+  productInfo: {
+    productName: string;
+    image: string;
+    price: string;
+    brand: string;
+  }
+): Promise<Array<any>> {
+  let productName = productInfo.productName;
+  let productVariations = [];
+
+  productName = productName.replace(/\d+GB\s*\|\s*\d+GB/g, "");
+
+  console.log(productName, "product name");
+
+  const { data: variationValuesFromSelect } = await tryCatch(
+    productPage.$$eval(
+      `table.variations td.value select[id='${variations}'] option`,
+      (el) =>
+        el
+          .map((el) => el.textContent)
+          .filter((itm) => itm !== "Choose an option")
+    ),
+    "from variationValues"
+  );
+
+  if (
+    variationValuesFromSelect !== null &&
+    variationValuesFromSelect.every((vartn) => vartn !== null)
+  ) {
+    let prevPriceRead = "";
+    for (let i = 0; i <= variationValuesFromSelect.length - 1; i++) {
+      Promise.all([
+        await tryCatch(
+          productPage.waitForSelector(
+            `table.variations select[id='${variations}`
+          ),
+          "3?"
+        ),
+        await tryCatch(
+          productPage.click(`table.variations select[id='${variations}']`),
+          "1?"
+        ),
       ]);
 
-      allGPUs = [...allGPUs, ...producList];
-      i++;
-    }
+      await tryCatch(
+        productPage.select(
+          `table.variations select[id='${variations}']`,
+          variationValuesFromSelect[i]
+        ),
+        "2?"
+      );
 
-    return allGPUs;
-  } catch (error) {
-    if (error instanceof Error) {
-      console.log(error.message);
+      await tryCatch(
+        productPage.waitForSelector(
+          "div.woocommerce-variation-price span.electro-price ins span bdi"
+        ),
+        "4?"
+      );
+
+      if (prevPriceRead !== "") {
+        await productPage.waitForFunction(
+          (sel, prev) => {
+            const el = document.querySelector(sel);
+
+            return el && el.textContent!.trim() !== prev;
+          },
+          { polling: "mutation", timeout: 6000 }, // optional options
+          "div.woocommerce-variation-price span.electro-price ins span bdi",
+          prevPriceRead
+        );
+      }
+
+      const { data: price } = await tryCatch(
+        productPage.$eval(
+          "div.woocommerce-variation-price span.electro-price ins span bdi",
+          (price) => price?.textContent
+        ),
+        `?5 ${originalName}`
+      );
+
+      if (price !== null) {
+        prevPriceRead = price;
+      }
+
+      productVariations.push({
+        ...productInfo,
+        originalName,
+        productName: `${finalRemovalUnnecessaryKeywords(productName)} ${
+          variationValuesFromSelect[i]
+        }`,
+        price,
+      });
     }
   }
+  return productVariations;
 }
 
-const groq = new Groq({ apiKey: process.env.GROQ_KEY! });
+function unifyVariationToName(variation: string, str: string): any {
+  return "";
+}
 
-async function getProductDetails(gpuNames: Array<string>) {
-  return groq.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: `I have this list of GPU names: ${gpuNames.join(
-          " **** "
-        )} they are separated with this separator ' *** '. I want you to find each details on that list in this form:
-          {
-            "gpuModel": string,
-            "brand": string,
-            "manufacturer": string,
-            "productName": string,
-            "partNumber": string,
-            "gpuArchitecture": string,
-            "cudaCoreCount":number,
-            "memoryType": string,
-            "memoryCapacity":number,
-            "memoryBusWidth":number,
-            "memoryBandwidth":number,
-            "baseClock":number,
-            "boostClock":number,
-            "memoryClock":number,
-            "directXSupport":number,
-            "openGLSupport":float,
-            "vulkanSupport":float,
-            "displayPorts":number,
-            "hdmiPorts":number,
-            "hdmiVersion":float,
-            "displayPortVersion":string,
-            "powerConsumption":number,
-            "recommendedPowerSupply":number,
-            "coolingSystem": string,
-            "cardLength":number,
-            "cardHeight":number,
-            "cardThickness":number,
-            "supportedTechnologies": Array of string listing all gpu supported technlogies,
-            "uniqueFeatures":Array of string for all gpu unique features
-        }. Response should be in JSON with 'gpus' property in the form of array
-        `,
-      },
-    ],
-    model: "llama-3.1-8b-instant",
-    response_format: {
-      type: "json_object",
-    },
+function initialRemoveUnnecessaryKeywords(str: string): string {
+  return str
+    .replace("Graphics Card", "")
+    .replace("Video Card", "")
+    .replace("GeForce", "")
+    .replace(/GDDR\d+X?/i, "");
+}
+
+function finalRemovalUnnecessaryKeywords(str: string): string {
+  return str
+    .replace(/\s{2,}/, " ")
+    .replace("|", "")
+    .replace(/\d+\s*-?\s*bit/i, "")
+    .replace(/\s{2,}/, " ")
+    .trim();
+}
+
+function allProductsEvalCallback(products: HTMLElement[]) {
+  return products.map((product) => {
+    const linkSelector =
+      "div.product-loop-header.product-item__header a.woocommerce-LoopProduct-link.woocommerce-loop-product__link";
+    return {
+      link: product.querySelector(linkSelector)?.getAttribute("href"),
+    } as any;
   });
 }
+
+function createDelay(duration: number) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(true);
+    }, duration);
+  });
+}
+
+(async function () {
+  const startTime = Date.now();
+
+  await getAllData();
+
+  const endTime = Date.now();
+  const durationMilliseconds = endTime - startTime;
+  const durationSeconds = (durationMilliseconds / 1000).toFixed(2);
+  console.log(durationSeconds, "durationSec");
+})();
