74cfed17827ef4894a15bb074c07cf937d198478
Rhyz <arongrhyz@gmail.com>
2025-07-07 03:24:10 +0800

    modified db config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
+  max: 5,
 });
-const db = drizzle({ client: pool });
+export const db = drizzle({ client: pool });
